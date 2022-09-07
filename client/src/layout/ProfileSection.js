4e087e55eb97afdad50dd1c4eb9643c3d47b2f33
satyam-negi <satyamnegi2009@gmail.com>
2022-09-07 10:49:28 +0530

    userDetails endpoint

--- client/src/layout/ProfileSection.js
+++ client/src/layout/ProfileSection.js
@@ -20,7 +20,7 @@ function ProfileSection(props) {
     //props.userData.dpUrl = `https://www.gravatar.com/avatar/${MD5(props.userData.email)}?d=wavatar&s=1000&r=x`;
 
     useEffect(() => {
-        props.userData.dpUrl = `https://www.gravatar.com/avatar/${MD5(props.userData.email)}?d=identicon&s=10&r=x`
+        props.userData.dpUrl = `https://www.gravatar.com/avatar/${MD5(props.userData.email)}?d=wavatar&s=10&r=x`
         console.log(props.userData.dpUrl);
     }, [])
 
@@ -228,7 +228,7 @@ function ProfileSection(props) {
 
                     <div className="top-tape"></div>
                     <div className="user-avatar">
-                        {props.userData && <img src='https://www.gravatar.com/avatar/242600846a716588f455fbb330e2d035?d=identicon&s=1000&r=x' />}
+                        {props.userData && <img src='https://www.gravatar.com/avatar/242600846a716588f455fbb330e2d035?d=wavatar&s=1000&r=x' />}
                     </div>
                     <div className="user-info">
                         <h2>{props.userData.name}</h2>
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,19 +3,30 @@ const mysql = require("mysql");
 const router = Router();
 
 const db = mysql.createConnection({
-    user: "agytYNOlUB",
-    host: "remotemysql.com",
-    password: "FA5BTMxjBs",
-    database: "agytYNOlUB",
-    
-    // user: "root",
-    // host: "localhost",
-    // password: "admin",
-    // database: "cep",
+    // user: "agytYNOlUB",
+    // host: "remotemysql.com",
+    // password: "FA5BTMxjBs",
+    // database: "agytYNOlUB",
+    user: "root",
+    host: "localhost",
+    password: "password",
+    database: "cep",
 });
 
 router.post("/details", (request, response) => {
-    
+    // console.log(request);
+    const email = request.body.userEmail;
+    db.query("select * from users where email=?",
+    [email],
+    (error,result)=>{
+        if(error) console.log(error);
+        else {
+            if(result.length==0)
+                response.send({statusCode: 404, message: "user not found", data:null});
+            else
+                response.send({statusCode: 300, message: "user details found", data:result});
+        }
+    });
 })
 
 module.exports = router
